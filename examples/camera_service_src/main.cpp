a96b1902d32d428e96144e897dde724e2d498939
Abay11 <abaev.alim@yandex.ru>
2022-10-23 21:09:28 +0300

    Fixes in main.cpp (camera part)

--- examples/camera_service_src/main.cpp
+++ examples/camera_service_src/main.cpp
@@ -20,7 +20,6 @@ int main(int argc, char* argv[])
 {
 	using boost::asio::ip::tcp;
 
-	std::cout << "Started" << std::endl;
 	mylog::init();
 
 	boost::asio::io_service ioservice;
@@ -31,10 +30,10 @@ int main(int argc, char* argv[])
 		po::options_description config{"Options"};
 		config.add_options()
 		("help", "Help screen")
-		("cloud-ip", po::value<std::string>()->required(), "Cloud server's IP")
-		("http2-port", po::value<std::string>()->required(), "HTTP2 port")
+		("cloud-ip", po::value<std::string>(), "Cloud server's IP")
+		("http2-port", po::value<std::string>(), "HTTP2 port")
 		("http2-protocol", po::value<std::string>()->default_value("tls"), "'tls' or 'plain'")
-		("camera-ip", po::value<std::string>()->required(), "Camera IP")
+		("camera-ip", po::value<std::string>(), "Camera IP")
 		("camera-http-port", po::value<std::string>()->default_value("80"), "HTTP port")
 		("camera-rtsp-port", po::value<std::string>()->default_value("554"), "RTSP port");
 
@@ -42,7 +41,10 @@ int main(int argc, char* argv[])
 		po::store(parse_command_line(argc, argv, config), vm);
 		po::notify(vm);
 
-		if(vm.count("help"))
+		if(vm.count("help")
+			|| !vm.count("cloud-ip")
+			|| !vm.count("http2-port")
+			|| !vm.count("camera-ip"))
 		{
 			std::cerr << config << std::endl;
 			return 1;
@@ -50,6 +52,7 @@ int main(int argc, char* argv[])
 		
 		validate(vm);
 
+		std::cout << "Started" << std::endl;
 		BOOST_LOG_TRIVIAL(info) << "*** New run ***";
 
 		boost::asio::io_context io_context;
